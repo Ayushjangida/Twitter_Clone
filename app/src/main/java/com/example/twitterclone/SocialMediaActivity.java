f6777554e4599b6c0aff3820ad255f80e78b873a
Ayush <ayushjangida30@gmail.com>
2020-08-18 00:00:48 +0530

    Set up Social Media Activity

--- app/src/main/java/com/example/twitterclone/SocialMediaActivity.java
+++ app/src/main/java/com/example/twitterclone/SocialMediaActivity.java
@@ -3,19 +3,62 @@ package com.example.twitterclone;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.ProgressDialog;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.TextView;
 
+import com.parse.FindCallback;
+import com.parse.Parse;
+import com.parse.ParseException;
+import com.parse.ParseQuery;
 import com.parse.ParseUser;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class SocialMediaActivity extends AppCompatActivity {
+
+    private ListView listView;
+    private List<String> userList = new ArrayList<>();
+    private ArrayAdapter adapter;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_social_media);
+        listView = findViewById(R.id.list_view_soical_media_activity);
+        adapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1, userList);
+        ProgressDialog progressDialog = new ProgressDialog(this);
+        progressDialog.setMessage("Loading Users...");
+
+        getUsers();
+    }
+
+    private void getUsers() {
+        final ProgressDialog progressDialog = new ProgressDialog(this);
+        progressDialog.setMessage("Loading Users...");
+        progressDialog.show();
+        ParseQuery<ParseUser> parseQuery = ParseUser.getQuery();
+        parseQuery.whereNotEqualTo("username", ParseUser.getCurrentUser().get("username"));
+        parseQuery.findInBackground(new FindCallback<ParseUser>() {
+            @Override
+            public void done(List<ParseUser> objects, ParseException e) {
+                if (objects.size() > 0 && e == null)    {
+                    for (ParseUser user : objects)  {
+                        userList.add(user.getUsername());
+                    }
+                    progressDialog.dismiss();
+                    listView.setAdapter(adapter);
+                }
+            }
+        });
+
     }
 
     @Override
@@ -35,5 +78,6 @@ public class SocialMediaActivity extends AppCompatActivity {
             startActivity(intent);
         }
         return super.onOptionsItemSelected(item);
+
     }
 }
